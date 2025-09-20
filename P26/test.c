d570555dbd0088845a2b3c9b7ed8b76daf4b66dd
P1y-ush <sharma.piyush4019@gmail.com>
2025-09-21 00:12:41 +0530

    commit

--- P26/test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-    printf("Hi! %s\n", argv[1]);
-    return 0;
-}
--- P30/myscript.sh
+++
@@ -1,1 +0,0 @@
-echo "Script ran at $(date)" >> /home/piyush/Desktop/SS/P30/output.txt
